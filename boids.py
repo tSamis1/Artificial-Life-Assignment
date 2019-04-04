781ff633fed48b65cb9fa9c2bc6e2d615b956dc5
Thomas Hardy <thestaffofmoses@gmail.com>
2019-04-03 22:47:13 -0400

    Update boids.py

--- boids.py
+++ boids.py
@@ -32,7 +32,7 @@ def build_graph():
     root.geometry('%dx%d+%d+%d' % (
     WIDTH, HEIGHT, (root.winfo_screenwidth() - WIDTH) / 2, (root.winfo_screenheight() - HEIGHT) / 2))
     root.bind_all('<Escape>', lambda event: event.widget.quit())
-    graph = Canvas(root, width=WIDTH, height=HEIGHT, background='white')
+    graph = Canvas(root, width=WIDTH, height=HEIGHT, background='black')
     graph.after(40, update)
     graph.pack()
 
